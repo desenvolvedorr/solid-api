4b1ad2ee9a6fb6c65123df7b50f7183bb739b331
Rouseman Brito <rouseman@Mys-MacBook-Pro.local>
2020-08-13 19:05:01 -0300

    first commit

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,32 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail;
+    
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth: {
+                user: 'd03f184c1c663e',
+                pass: 'ba2c3c272e4fa7'
+            },
+        })
+    }
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            }, 
+            subject: message.subject,
+            html: message.body,
+        })
+    }
+}
